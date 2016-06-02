18742d173c0066d55be6b1fcfbc9179c669e1a04
Marquinhus Gonçalves <eu@marquinhusgoncalves.com>
2016-06-02 11:40:47 -0300

    End of the first reading

--- node.js
+++ node.js
@@ -1,2 +1,26 @@
-var mensagem = "Olá mundo";
-console.log(mensagem);
+// var mensagem = "Olá mundo";
+// console.log(mensagem);
+
+//Subir servidor Node
+var http = require("http");
+var server = http.createServer(function(request, response) {
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("<h1>Hello!</h1><p>You asked for <code>" +
+  request.url + "</code></p>");
+  response.end();
+});
+server.listen(8000);
+//abra no navegador a url http://localhost:8000/hello
+
+//Para agir como um cliente HTTP, nós podemos usar a função request no módulo "http".
+var http = require("http");
+var request = http.request({
+  hostname: "eloquentjavascript.net",
+  path: "/20_node.html",
+  method: "GET",
+  headers: {Accept: "text/html"}
+}, function(response) {
+  console.log("Server responded with status code",
+              response.statusCode);
+});
+request.end();
